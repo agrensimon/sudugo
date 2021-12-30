c7f20ae5366e880336783534a763780037b86047
Simon Ågren <simon.agren@gmail.com>
2021-12-30 19:52:24 +0100

    working recursive suduko solver

---
+++ main.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type suduko struct {
+	board            [][]int
+	possibleRowMoves map[int][]int
+}
+
+func main() {}
+
+func (s *suduko) solve(cursor int) bool {
+	if cursor >= 9*9 {
+		return true
+	}
+
+	// skip already occupied cells
+	for s.board[cursor/9][cursor%9] != 0 {
+		cursor++
+	}
+
+	for n := 1; n <= 9; n++ {
+		s.board[cursor/9][cursor%9] = n
+		if s.check(cursor/9, cursor%9) && s.solve(cursor+1) {
+			return true
+		}
+		s.board[cursor/9][cursor%9] = 0
+	}
+	return false
+}
+
+func (s *suduko) check(row, col int) bool {
+	return s.checkRow(row) && s.checkCol(col) && s.checkSubGrid(row, col)
+}
+
+func (s *suduko) checkSubGrid(row, col int) bool {
+	rowStart := (row / 3) * 3
+	colStart := (col / 3) * 3
+
+	occured := map[int]bool{}
+	for r := rowStart; r < rowStart+3; r++ {
+		for c := colStart; c < colStart+3; c++ {
+			if s.board[r][c] == 0 {
+				continue
+			}
+			if s.board[r][c] != 0 && occured[s.board[r][c]] {
+				return false
+			}
+			occured[s.board[r][c]] = true
+		}
+	}
+
+	return true
+}
+
+func (s *suduko) checkRow(row int) bool {
+	occured := map[int]bool{}
+	for c := 0; c < 9; c++ {
+		if s.board[row][c] == 0 {
+			continue
+		}
+		if occured[s.board[row][c]] {
+			return false
+		}
+		occured[s.board[row][c]] = true
+	}
+	return true
+}
+
+func (s *suduko) checkCol(col int) bool {
+	occured := map[int]bool{}
+	for r := 0; r < 9; r++ {
+		if s.board[r][col] == 0 {
+			continue
+		}
+		if occured[s.board[r][col]] {
+			return false
+		}
+		occured[s.board[r][col]] = true
+	}
+	return true
+}
+
+func newSuduko() suduko {
+	s := suduko{}
+	s.board = make([][]int, 9)
+	for i := 0; i < 9; i++ {
+		s.board[i] = make([]int, 9)
+		s.possibleRowMoves[i] = append(s.possibleRowMoves[i], []int{1, 2, 3, 4, 5, 6, 7, 8, 9}...)
+	}
+	return s
+}
+
+func (s *suduko) dump() {
+	for y, row := range s.board {
+		if y%3 == 0 {
+			fmt.Println("+---------+---------+---------+")
+		}
+		for x, s := range row {
+			if x%3 == 0 {
+				fmt.Print("|")
+			}
+
+			switch s {
+			case 0:
+				fmt.Print(" . ")
+			default:
+				fmt.Printf(" %v ", s)
+			}
+		}
+		fmt.Println("|")
+	}
+	fmt.Println("+---------+---------+---------+")
+}
+
+func parse(path string) (begin suduko, goal suduko, err error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return suduko{}, suduko{}, err
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	begin = newSuduko()
+	goal = newSuduko()
+	row, col := 0, 0
+	for scanner.Scan() {
+		split := strings.Split(scanner.Text(), "")
+		if len(split) == 0 {
+			break
+		}
+		for _, r := range split {
+			i, err := strconv.Atoi(string(r))
+			if err != nil {
+				return suduko{}, suduko{}, err
+			}
+			begin.board[row][col] = i
+			col++
+		}
+		col = 0
+		row++
+	}
+
+	row, col = 0, 0
+	for scanner.Scan() {
+		split := strings.Split(scanner.Text(), "")
+		if len(split) == 0 {
+			break
+		}
+		for _, r := range split {
+			i, err := strconv.Atoi(string(r))
+			if err != nil {
+				return suduko{}, suduko{}, err
+			}
+			goal.board[row][col] = i
+			col++
+		}
+		col = 0
+		row++
+	}
+	return
+}
+
+func (s *suduko) getMove(row int) int {
+	move := s.possibleRowMoves[row][0]
+	s.possibleRowMoves[row][0] = 0
+	s.possibleRowMoves[row] = s.possibleRowMoves[row][1:]
+	return move
+}
---
+++ main_test.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	wantBegin := newSuduko()
+	wantBegin.board[0][0] = 1
+	wantBegin.board[3][3] = 2
+	wantBegin.board[6][6] = 3
+
+	wantGoal := newSuduko()
+	wantGoal.board[1][1] = 1
+	wantGoal.board[4][4] = 2
+	wantGoal.board[7][7] = 3
+
+	gotBegin, gotGoal, err := parse("./testdata/testRead")
+	if err != nil {
+		t.Fatal("unexpected error")
+	}
+
+	if !reflect.DeepEqual(gotBegin, wantBegin) {
+		gotBegin.dump()
+		wantBegin.dump()
+		t.Fatal("begin sudukos do not match")
+	}
+	if !reflect.DeepEqual(gotGoal, wantGoal) {
+		gotGoal.dump()
+		wantGoal.dump()
+		t.Fatal("goal sudukos do not match")
+
+	}
+}
+func TestCheckSubGrid(t *testing.T) {
+
+	tests := []struct {
+		mx, my, m []int
+		want      bool
+	}{
+		{
+			mx:   []int{0, 1, 2},
+			my:   []int{0, 1, 2},
+			m:    []int{1, 2, 3},
+			want: true,
+		},
+		{
+			mx:   []int{0, 1, 2},
+			my:   []int{0, 1, 2},
+			m:    []int{1, 1, 3},
+			want: false,
+		},
+		{
+			mx:   []int{8, 8, 8},
+			my:   []int{0, 1, 2},
+			m:    []int{1, 1, 3},
+			want: false,
+		},
+		{
+			mx:   []int{8, 8, 8},
+			my:   []int{7, 6, 8},
+			m:    []int{8, 7, 6},
+			want: true,
+		},
+		{
+			mx:   []int{8, 8, 8},
+			my:   []int{7, 6, 8},
+			m:    []int{6, 7, 6},
+			want: false,
+		},
+	}
+
+	for _, tc := range tests {
+		s := newSuduko()
+		for i := range tc.m {
+			s.board[tc.my[i]][tc.mx[i]] = tc.m[i]
+		}
+
+		if tc.want != s.checkSubGrid(tc.my[0], tc.mx[0]) {
+			s.dump()
+			t.Fatal()
+		}
+	}
+}
+
+func TestUnsolvable(t *testing.T) {
+	tests := []struct {
+		inputPath string
+	}{
+		{inputPath: "./testdata/test_03"},
+	}
+
+	for _, tc := range tests {
+		got, want, err := parse(tc.inputPath)
+		if err != nil {
+			t.Fatal("unexpected error")
+		}
+
+		if got.solve(0) {
+			t.Fatal("got a solution to unsolvable puzzle")
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			fmt.Println("got:")
+			got.dump()
+			fmt.Println("want:")
+			want.dump()
+			t.Fatal("boards not same")
+		}
+	}
+}
+func TestSolvable(t *testing.T) {
+	tests := []struct {
+		inputPath string
+	}{
+		{inputPath: "./testdata/test_01"},
+		{inputPath: "./testdata/test_02"},
+	}
+
+	for _, tc := range tests {
+		got, want, err := parse(tc.inputPath)
+		if err != nil {
+			t.Fatal("unexpected error")
+		}
+
+		if !got.solve(0) {
+			got.dump()
+			t.Fatal("unable to solve")
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			fmt.Println("got:")
+			got.dump()
+			fmt.Println("want:")
+			want.dump()
+			t.Fatal("boards not same")
+		}
+	}
+}
